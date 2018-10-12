05589b9847950b5956fd213a658777c6ab1540d6
whentze <muvlon@hentzes.de>
2018-10-12 08:55:30 +0200

    API overhaul: Mutex no longer mandatory, instead there's now a !Sync singleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,155 @@
 #![feature(dbg_macro)]
 
-use lazy_static::lazy_static;
 use louis_sys::ThreadUnsafetyToken;
+use std::cell::Cell;
 use std::ffi::{CStr, CString};
-use std::mem::drop;
-use std::os::raw::{c_int, c_uint};
+use std::marker::PhantomData;
+use std::os::raw::{c_char, c_int, c_uint};
 use std::path::Path;
-use std::sync::Mutex;
 
 pub mod mode;
 
-lazy_static! {
-    pub static ref TOKEN: Mutex<ThreadUnsafetyToken> =
-        Mutex::new(ThreadUnsafetyToken::take().expect("Could not get ThreadUnsafetyToken."));
-}
-
 type LouisString = widestring::UCString<louis_sys::widechar>;
+const OUTLEN_MULTIPLIER: c_int = 4 + 2 * std::mem::size_of::<louis_sys::widechar>() as c_int;
 
-pub fn liblouis_version() -> Result<semver::Version, semver::SemVerError> {
-    let guard = TOKEN.lock();
-    let version_str = unsafe { CStr::from_ptr(louis_sys::lou_version()) }
-        .to_str()
-        .unwrap();
-    drop(guard);
-    semver::Version::parse(version_str)
+/// A singleton that handles all access to liblouis.
+///
+/// This struct is needed since liblouis is thread-unsafe and can only be called from one thread at a time.
+/// It is `Send`, but `!Sync`, so that at any given moment, all `&`s to it live on the same thread,
+/// but if you own it you can move it across threads. All liblouis calls therefore need a `&` to this.
+/// It also provides some convenient setup/teardown logic:
+/// - When created, it registers a logging callback with liblouis that
+///   pipes all messages into the log.rs facade with the appropriate log levels set
+/// - When dropped, it resets liblouis' logging behaviour to the default and calls `lou_free()`
+///   to make sure no memory is leaked.
+pub struct Louis {
+    _token: ThreadUnsafetyToken,
+    nosync: PhantomData<Cell<u8>>,
 }
 
-pub fn list_tables() -> Vec<String> {
-    let guard = TOKEN.lock();
-    let list_begin = unsafe { louis_sys::lou_listTables() };
-    drop(guard);
-    let mut res = Vec::new();
-    for offset in 0.. {
-        let ptr = unsafe { *(list_begin.offset(offset)) };
-        if ptr == std::ptr::null() {
-            break;
-        }
-        let table_name = Path::new(unsafe { CStr::from_ptr(ptr) }.to_str().unwrap())
-            .file_name()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_owned();
-        res.push(table_name);
+impl Louis {
+    /// Tries to initialize liblouis, returning `Some(Louis)` on success.
+    /// On failure, it returns `None`, indicating that the ThreadUnsafetyToken has already been taken.
+    pub fn new() -> Option<Self> {
+        ThreadUnsafetyToken::take().map(|_token| {
+            let louis = Louis {
+                _token,
+                nosync: PhantomData,
+            };
+            louis.configure_logging();
+            louis
+        })
     }
-    res
+
+    /// Returns the version of liblouis that this crate is linked against
+    pub fn version(&self) -> Result<semver::Version, semver::SemVerError> {
+        let version_str = unsafe { CStr::from_ptr(louis_sys::lou_version()) }
+            .to_str()
+            .unwrap();
+        semver::Version::parse(version_str)
+    }
+
+    /// Lists the filenames of all the tables that are available
+    pub fn list_tables(&self) -> Vec<String> {
+        let list_begin = unsafe { louis_sys::lou_listTables() };
+        let mut res = Vec::new();
+        for offset in 0.. {
+            let ptr = unsafe { *(list_begin.offset(offset)) };
+            if ptr == std::ptr::null() {
+                break;
+            }
+            let table_name = Path::new(unsafe { CStr::from_ptr(ptr) }.to_str().unwrap())
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_owned();
+            res.push(table_name);
+        }
+        res
+    }
+
+    /// Translates the text in `input` according to the table given by `table_name`.
+    /// 
+    /// # Examples
+    ///
+    /// Pass `mode=0` for regular translation:
+    /// 
+    /// ```
+    /// # use louis::Louis;
+    /// let louis = Louis::new().unwrap();
+    /// let brl = louis.translate_simple("ru.tbl", "Я понимаю", 0);
+    /// assert_eq!(brl, "$ PONIMA|");
+    /// ```
+    /// 
+    /// You can also translate directly to Unicode Braille dots:
+    /// 
+    /// ```
+    /// # use louis::{Louis, mode::DOTS_UNICODE};
+    /// # let louis = Louis::new().unwrap();
+    /// let dots = louis.translate_simple("sr.tbl", "Добродошли", DOTS_UNICODE);
+    /// assert_eq!(dots, "⠨⠙⠕⠃⠗⠕⠙⠕⠱⠇⠊");
+    /// ```
+    pub fn translate_simple(
+        &self,
+        table_name: &str,
+        input: &str,
+        mode: mode::TranslationMode,
+    ) -> String {
+        let inbuf = LouisString::from_str(input).unwrap();
+        let mut inlen = inbuf.len() as c_int;
+
+        let mut outlen = inlen * OUTLEN_MULTIPLIER;
+        let mut outvec = Vec::with_capacity(outlen as usize);
+        let outptr = outvec.as_mut_ptr();
+
+        unsafe {
+            louis_sys::lou_translateString(
+                CString::new(table_name).unwrap().as_ptr(),
+                inbuf.as_ptr(),
+                &mut inlen as *mut _,
+                outptr,
+                &mut outlen as *mut _,
+                std::ptr::null::<louis_sys::formtype>() as *mut _,
+                std::ptr::null::<std::os::raw::c_char>() as *mut _,
+                mode,
+            )
+        };
+
+        std::mem::forget(outvec);
+        let outbuf = unsafe { LouisString::from_ptr(outptr, outlen as usize) }.unwrap();
+        outbuf.to_string().unwrap()
+    }
+
+    fn configure_logging(&self) {
+        unsafe {
+            louis_sys::lou_setLogLevel(filter_to_lou_loglevel(log::STATIC_MAX_LEVEL));
+            louis_sys::lou_registerLogCallback(Some(log_callback));
+        };
+    }
+
+    fn reset_logging(&self) {
+        unsafe {
+            louis_sys::lou_setLogLevel(louis_sys::logLevels_LOG_INFO);
+            louis_sys::lou_registerLogCallback(None);
+        };
+    }
 }
 
-const OUTLEN_MULTIPLIER : c_int = 4 + 2*std::mem::size_of::<louis_sys::widechar>() as c_int;
-
-pub fn translate_simple(table_name: &str, input: &str, mode: mode::TranslationMode) -> String {
-    let inbuf = LouisString::from_str(input).unwrap();
-    let mut inlen = inbuf.len() as c_int;
-
-    let mut outlen = inlen * OUTLEN_MULTIPLIER;
-    let mut outvec = Vec::with_capacity(outlen as usize);
-    let outptr = outvec.as_mut_ptr();
-
-    let guard = TOKEN.lock();
-    unsafe {
-        louis_sys::lou_translateString(
-            CString::new(table_name).unwrap().as_ptr(),
-            inbuf.as_ptr(),
-            &mut inlen as *mut _,
-            outptr,
-            &mut outlen as *mut _,
-            std::ptr::null::<louis_sys::formtype>() as *mut _,
-            std::ptr::null::<std::os::raw::c_char>() as *mut _,
-            mode,
-        )
-    };
-    drop(guard);
-
-    std::mem::forget(outvec);
-    let outbuf = unsafe { LouisString::from_ptr(outptr, outlen as usize) }.unwrap();
-    outbuf.to_string().unwrap()
+impl Drop for Louis {
+    fn drop(&mut self) {
+        self.reset_logging();
+        unsafe { louis_sys::lou_free() };
+    }
 }
 
-fn lou_loglevel_to_level(level: c_uint) -> log::Level{
+fn lou_loglevel_to_level(level: c_uint) -> log::Level {
     match level {
-        0...louis_sys::logLevels_LOG_ALL   => log::Level::Trace,
+        0...louis_sys::logLevels_LOG_ALL => log::Level::Trace,
         0...louis_sys::logLevels_LOG_DEBUG => log::Level::Debug,
-        0...louis_sys::logLevels_LOG_INFO  => log::Level::Info,
-        0...louis_sys::logLevels_LOG_WARN  => log::Level::Warn,
-        _                                  => log::Level::Error,
+        0...louis_sys::logLevels_LOG_INFO => log::Level::Info,
+        0...louis_sys::logLevels_LOG_WARN => log::Level::Warn,
+        _ => log::Level::Error,
     }
 }
 
@@ -91,23 +157,14 @@ fn filter_to_lou_loglevel(filter: log::LevelFilter) -> c_uint {
     match filter {
         log::LevelFilter::Trace => louis_sys::logLevels_LOG_ALL,
         log::LevelFilter::Debug => louis_sys::logLevels_LOG_DEBUG,
-        log::LevelFilter::Info  => louis_sys::logLevels_LOG_INFO,
-        log::LevelFilter::Warn  => louis_sys::logLevels_LOG_WARN,
+        log::LevelFilter::Info => louis_sys::logLevels_LOG_INFO,
+        log::LevelFilter::Warn => louis_sys::logLevels_LOG_WARN,
         log::LevelFilter::Error => louis_sys::logLevels_LOG_ERROR,
-        log::LevelFilter::Off   => louis_sys::logLevels_LOG_OFF,
+        log::LevelFilter::Off => louis_sys::logLevels_LOG_OFF,
     }
 }
 
-pub fn enable_logging() {
-    let guard = TOKEN.lock();
-    unsafe {
-        louis_sys::lou_setLogLevel(filter_to_lou_loglevel(log::STATIC_MAX_LEVEL));
-        louis_sys::lou_registerLogCallback(Some(log_callback));
-    };
-    drop(guard);
-}
-
-unsafe extern "C" fn log_callback(level: louis_sys::logLevels, message: *const ::std::os::raw::c_char) {
+unsafe extern "C" fn log_callback(level: louis_sys::logLevels, message: *const c_char) {
     let message_str = CStr::from_ptr(message).to_string_lossy();
     log::log!(target: "liblouis", lou_loglevel_to_level(level), "{}", message_str);
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,53 +1,73 @@
-use super::translate_simple;
+use super::Louis;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref API: Mutex<Louis> = Mutex::new(Louis::new().unwrap());
+}
 
 #[test]
 fn louis_version() {
-    use super::liblouis_version;
     use semver::Version;
-    assert!(liblouis_version() >= Version::parse("3.0.0"));
+    let louis = API.lock().unwrap();
+    assert!(louis.version() >= Version::parse("3.0.0"));
 }
 
 #[test]
 fn list_tables() {
-    use super::list_tables;
-    let tables = list_tables();
+    let louis = API.lock().unwrap();
+    let tables = louis.list_tables();
     assert!(tables.len() > 0);
 }
 
 #[test]
 fn translate_simple_de() {
     let sentence = "Dies ist ein kurzer Satz.";
-    assert_eq!(translate_simple("de.tbl", sentence, 0), "d0s } 6 kz7 sz.");
+    let louis = API.lock().unwrap();
+    assert_eq!(
+        louis.translate_simple("de.tbl", sentence, 0),
+        "d0s } 6 kz7 sz."
+    );
 }
 
 #[test]
 fn translate_simple_en() {
     let sentence = "This is an example sentence with a rare word: syzygy.";
-    assert_eq!(translate_simple("en_US.tbl", sentence, 0), ",? is an example s5t;e )a r>e ~w3 syzygy4");
+    let louis = API.lock().unwrap();
+    assert_eq!(
+        louis.translate_simple("en_US.tbl", sentence, 0),
+        ",? is an example s5t;e )a r>e ~w3 syzygy4"
+    );
 }
 
 #[test]
 fn translate_simple_escape_umlauts() {
     let sentence = "äöü";
-    assert_eq!(translate_simple("en_US.tbl", sentence, 0), "`a`o`u");
+    let louis = API.lock().unwrap();
+    assert_eq!(louis.translate_simple("en_US.tbl", sentence, 0), "`a`o`u");
 }
 
 #[test]
 fn translate_simple_miss_everything() {
     let sentence = "はたらく細胞";
-    assert_eq!(translate_simple("en_US.tbl", sentence, 0), r"'\x306f''\x305f''\x3089''\x304f''\x7d30''\x80de'");
+    let louis = API.lock().unwrap();
+    assert_eq!(
+        louis.translate_simple("en_US.tbl", sentence, 0),
+        r"'\x306f''\x305f''\x3089''\x304f''\x7d30''\x80de'"
+    );
 }
 
 #[test]
 fn translate_simple_dots_unicode() {
     use super::mode::DOTS_UNICODE;
     let sentence = "Turn this sentence into braille dots please!";
-    assert_eq!(translate_simple("en_US.tbl", sentence, DOTS_UNICODE), "⠠⠞⠥⠗⠝⠀⠹⠀⠎⠢⠞⠰⠑⠀⠔⠖⠃⠗⠇⠀⠙⠕⠞⠎⠀⠏⠇⠂⠎⠑⠖");
+    let louis = API.lock().unwrap();
+    assert_eq!(louis.translate_simple("en_US.tbl", sentence, DOTS_UNICODE), "⠠⠞⠥⠗⠝⠀⠹⠀⠎⠢⠞⠰⠑⠀⠔⠖⠃⠗⠇⠀⠙⠕⠞⠎⠀⠏⠇⠂⠎⠑⠖");
 }
-
 
 #[test]
 fn translate_simple_empty() {
     let sentence = "";
-    assert_eq!(translate_simple("de.tbl", sentence, 0), "");
+    let louis = API.lock().unwrap();
+    assert_eq!(louis.translate_simple("de.tbl", sentence, 0), "");
 }
