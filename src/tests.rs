f03e91281819f2cbae58cd6d0ddb5a1ac1a93e87
whentze <muvlon@hentzes.de>
2018-10-12 02:30:02 +0200

    add simple test for english

--- src/tests.rs
+++ src/tests.rs
@@ -18,3 +18,17 @@ fn translate_simple_de() {
     let sentence = "Dies ist ein kurzer Satz.";
     assert_eq!(translate_simple("de.tbl", sentence), "d0s } 6 kz7 sz.");
 }
+
+#[test]
+fn translate_simple_en() {
+    use super::translate_simple;
+    let sentence = "This is an example sentence with a rare word: syzygy.";
+    assert_eq!(translate_simple("en_US.tbl", sentence), ",? is an example s5t;e )a r>e ~w3 syzygy4");
+}
+
+#[test]
+fn translate_simple_empty() {
+    use super::translate_simple;
+    let sentence = "";
+    assert_eq!(translate_simple("de.tbl", sentence), "");
+}
