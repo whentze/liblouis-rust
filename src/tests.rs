ea44d39c3b1ed993e4e740cf6d564ce90ad22b18
whentze <muvlon@hentzes.de>
2018-10-12 03:38:43 +0200

    consolidate imports

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,5 @@
+use super::translate_simple;
+
 #[test]
 fn louis_version() {
     use super::liblouis_version;
@@ -14,35 +16,30 @@ fn list_tables() {
 
 #[test]
 fn translate_simple_de() {
-    use super::translate_simple;
     let sentence = "Dies ist ein kurzer Satz.";
     assert_eq!(translate_simple("de.tbl", sentence), "d0s } 6 kz7 sz.");
 }
 
 #[test]
 fn translate_simple_en() {
-    use super::translate_simple;
     let sentence = "This is an example sentence with a rare word: syzygy.";
     assert_eq!(translate_simple("en_US.tbl", sentence), ",? is an example s5t;e )a r>e ~w3 syzygy4");
 }
 
 #[test]
 fn translate_simple_escape_umlauts() {
-    use super::translate_simple;
     let sentence = "äöü";
     assert_eq!(translate_simple("en_US.tbl", sentence), "`a`o`u");
 }
 
 #[test]
 fn translate_simple_miss_everything() {
-    use super::translate_simple;
     let sentence = "はたらく細胞";
     assert_eq!(translate_simple("en_US.tbl", sentence), r"'\x306f''\x305f''\x3089''\x304f''\x7d30''\x80de'");
 }
 
 #[test]
 fn translate_simple_empty() {
-    use super::translate_simple;
     let sentence = "";
     assert_eq!(translate_simple("de.tbl", sentence), "");
 }
