231c77f0fadf7da8d29821af9bee6b3d50dd0ffa
muvlon <muvlon@hentzes.de>
2018-10-17 03:32:39 +0200

    make build.rs build liblouis using autotools if needed

--- louis-sys/build.rs
+++ louis-sys/build.rs
@@ -1,20 +1,40 @@
+extern crate autotools;
 extern crate bindgen;
+#[macro_use]
+extern crate log;
+extern crate pkg_config;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-lib=louis");
+    let mut builder = bindgen::Builder::default().header("wrapper.h");
 
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .generate()
-        .unwrap_or_else(|_| {
-            bindgen::Builder::default()
-                .header("fallback.h")
-                .generate()
-                .expect("Error generating Bindings")
-        });
+    match pkg_config::Config::new()
+        .atleast_version("3.1.0")
+        .probe("liblouis")
+    {
+        Ok(system_liblouis) => {
+            info!(
+                "Found recent system liblouis via pkg-config. Version: {}",
+                system_liblouis.version
+            );
+        }
+        Err(e) => {
+            info!("pkg-config error while trying to detect liblouis: {}", e);
+            info!("building liblouis 3.7.0 from source");
+
+            let dest = autotools::build("liblouis-3.7.0");
+
+            env::set_var("PKG_CONFIG_PATH", dest.join("lib/pkgconfig"));
+            let our_liblouis = pkg_config::Config::new().atleast_version("3.7.0").probe("liblouis").unwrap();
+            for path in our_liblouis.include_paths {
+                builder = builder.clang_args(&["-I", path.parent().unwrap().to_str().unwrap()]);
+            }
+        }
+    };
+
+    let bindings = builder.generate().unwrap();
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
